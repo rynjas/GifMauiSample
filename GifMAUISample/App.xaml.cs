b325e06b660c55c734dbe985adc5e3f3e55824a2
Rajarajan <rynjas@gmail.com>
2022-06-27 23:00:21 +0530

    Sample Code

---
+++ GifMAUISample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GifMAUISample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ GifMAUISample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GifMAUISample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
